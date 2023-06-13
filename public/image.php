c2da3a92b1decc25c9a0bb256030cde4bb0aa988
begu0025 <emilia.beguin@etudiant.univ-reims.fr>
2023-06-13 16:39:59 +0200

    Mise en place de l'image par défaut en cas d'erreur

--- public/image.php
+++ public/image.php
@@ -14,10 +14,8 @@ try {
     $cover = Image::findById((int)$id);
     header('Content-Type: image/jpeg');
     echo $cover->getJpeg();
-} catch (ParameterException) {
-    http_response_code(400);
-} catch (EntityNotFoundException) {
-    http_response_code(404);
 } catch (Exception) {
-    http_response_code(500);
+    header('Content-Type: image/png');
+    $imageData = readfile("img/notFound.png");
+    echo $imageData;
 }
