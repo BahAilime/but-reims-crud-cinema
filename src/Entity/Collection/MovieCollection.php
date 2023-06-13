1a13796817f3b7ef671adcc315ab3b85664e45ba
begu0025 <emilia.beguin@etudiant.univ-reims.fr>
2023-06-13 10:40:12 +0200

    Création de MovieCollection.php et de sa fonction findAll

---
+++ src/Entity/Collection/MovieCollection.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Entity\Collection;
+
+use Database\MyPdo;
+use Entity\Movie;
+use PDO;
+
+class MovieCollection
+{
+    public static function findAll() : array
+    {
+        $sql = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            SELECT *
+            FROM movie;
+        SQL
+        );
+
+        $sql->execute();
+
+        return $sql->fetchAll(PDO::FETCH_CLASS, Movie::class);
+    }
+}
