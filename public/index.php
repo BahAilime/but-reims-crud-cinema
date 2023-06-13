24720e123ebefaa78d95651cbda096df2bfc81a2
begu0025 <emilia.beguin@etudiant.univ-reims.fr>
2023-06-13 11:53:21 +0200

    Correction title

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@ $html = new WebPage();
 $html->appendToHead("<meta http-equiv='X-UA-Compatible' content='IE=edge'>
   <meta name='viewport' content='width=device-width, initial-scale=1.0'>");
 
-$html->setTitle('Artistes');
+$html->setTitle('Films');
 $html->appendContent("<header><h1>Films</h1></header><main>");
 
 $movies = MovieCollection::findAll();
